9eceea2e100ee7769b4665b5a9fb8327f93e64af
Vladass22 <107876852+Vladass22@users.noreply.github.com>
2022-08-15 18:54:40 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 //Popup form
+	document.getElementById('intro_btn0').onclick = function() {
+		document.getElementById('main2').classList.add('pop-open');
+	}
 	document.getElementById('intro_btn').onclick = function() {
 		document.getElementById('main').classList.add('pop-open');
 		var $elem = document.querySelector('#bl3-ttl-1');
@@ -25,10 +28,13 @@
 	}
 	document.getElementById('pop_close').onclick = function() {
 		document.getElementById('main').classList.remove('pop-open');
+		document.getElementById('main2').classList.remove('pop-open');
 	}
 	document.getElementById('pop_close2').onclick = function() {
 		document.getElementById('main').classList.remove('pop-result');
 		document.getElementById('main').classList.remove('pop-open');
+		document.getElementById('main2').classList.remove('pop-result');
+		document.getElementById('main2').classList.remove('pop-open');
 	}
 
 
@@ -36,6 +42,7 @@
 //Validate form
 function validate(){
    document.getElementById('main').classList.add('pop-result');
+   document.getElementById('main2').classList.add('pop-result');
 
     return true;
 }
