9d60b31ce9ff8113a00c68a2001402b46fbd8712
Vladass22 <107876852+Vladass22@users.noreply.github.com>
2022-09-10 23:36:51 +0300

    Update mail2.php

--- mail2.php
+++ mail2.php
@@ -4,7 +4,7 @@
     $email = $_GET['Email']; // Вытаскиваем почту в переменную
     $tell = $_GET['Telegram/WhatsApp'];
     $message = "Форма обратной связи<br> Заявка на: VIP HUNTERS бесплатная регистрация<br> Имя: $name<br> Почта: $email<br> Telegram/WhatsApp: $tell"; // Формируем сообщение, отправляемое на почту
-    $to = "gptvinccorp@gmail.com, natalipbm@gmail.com, orenlr56@yandex.ru"; // Задаем получателя письма
+    $to = "natalipbm@gmail.com, orenlr56@yandex.ru"; // Задаем получателя письма
     $from = "Invest crypto evening"; // От кого пришло письмо
     $subject = "Письмо с сайта Invest crypto evening"; // Задаем тему письма
     $headers = "From: $from\r\nReply-To: $to\r\nContent-type: text/html; charset=utf-8\r\n"; // Формируем заголовок письма (при неправильном формировании может ломаться кодировка и т.д.)
